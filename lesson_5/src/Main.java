7f5018a9c0b2a9824afe5213421c7afa1999e6bf
eugenemoro <ugn.morozov@gmail.com>
2017-09-06 18:01:00 +0300

    Threads

---
+++ lesson_5/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+	static final int size = 10000000;
+	static final int h = size / 2;
+
+	public static void main(String[] args) {
+		float[] arr = new float[size];
+		for (int i = 0; i < arr.length ; i++) {
+			arr[i] = 1;
+		}
+		System.out.println("1-ый способ:");
+		method1(arr);
+		System.out.println("2-ой способ:");
+		method2(arr);
+	}
+
+	public static void method1(float[] arr){
+		long a = System.currentTimeMillis();
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+		}
+		System.out.println(System.currentTimeMillis() - a);
+	}
+
+	public static void method2(float[] arr){
+		float[] a1 = new float[h];
+		float[] a2 = new float[h];
+		long a = System.currentTimeMillis();
+		System.arraycopy(arr, 0, a1, 0, h);
+		System.out.println(System.currentTimeMillis() - a);
+		System.arraycopy(arr, h, a2, 0, h);
+		System.out.println(System.currentTimeMillis() - a);
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				method1(a1);
+			}
+		});
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				method1(a2);
+			}
+		});
+		System.arraycopy(a1, 0, arr, 0, h);
+		System.arraycopy(a2, 0, arr, h, h);
+		System.out.println(System.currentTimeMillis() - a);
+	}
+}
